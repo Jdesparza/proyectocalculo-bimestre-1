d1472b6c5bfae96ab27b31e46a2f452c72a3fed0
PabloM7 <50538339+PabloM7@users.noreply.github.com>
2019-05-14 16:03:35 -0500

    Parte realizada por Pablo Montaño

---
+++ ProyectoCalculo/src/importacion/Mensajes.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package importacion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Mensajes {
+    // Declaración de variables estáticas para importación
+    public static String fx = ("(-4x + x^3) / (x^2)");
+    public static String gx = ("(5x^3 - 3x + 3) / (x^3)");
+    public static String hx = ("8x - x^2 + 5");
+    public static String ix = ("5x - x^3 - 2^x");
+    public static String jx = ("3^x - 5x + 4");
+    public static String kx = ("√(x^2 - 6x) / (2x)");
+    public static String lx = ("2^x + 4x - x^3");
+    public static String mx = ("7x - √(2x) + 5");
+    public static String nx = ("4x^2 - 3^x - 8");
+    public static String ox = ("2(4^x) + x^-4 - 7");
+    // Declaración de variables estáticas con mensajes de entrada
+    public static String mensaje_opcion = "Por favor ingrese la opción: ";
+    
+    public static String mensaje_valorlimite = "Por favor ingrese el valor"
+            + " final para la variable x: ";
+    
+    public static String mensaje_valorinicial = "Por favor ingrese el valor "
+            + "inicial para la variable x: ";
+    
+    public static String mensaje_opcioninvalida = "La opción ingresada es "
+            + "incorrecta. Vuelva a ingresar la opción de acuerdo al Menú";
+    
+    public static String mensaje_g = "Gracias";
+}
